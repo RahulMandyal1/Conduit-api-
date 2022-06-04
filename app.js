2f52cdc0b322fd20d5419d1da002e2c699bf8be5
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-05 01:12:26 +0530

    added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var createError = require("http-errors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 //database connectivity
