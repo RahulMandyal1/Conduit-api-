dc5e332120232786235fb412234658bdbea1f87b
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-05 12:41:31 +0530

    Completed with the conduit api finished with deployment also

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 var express = require("express");
 var router = express.Router();
-const auth = require("../middlewares/auth");
-const User = require("../models/users");
-const Article = require("../models/articles");
 /* GET home page. */
-router.get("/", auth.isVerified, function (req, res, next) {
+router.get("/", function (req, res, next) {
   res.json({ message: " welcome to my blog app" });
 });
 module.exports = router;
